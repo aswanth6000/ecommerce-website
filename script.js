c7be92a940762c83dc91fd825840600a52553a60
Aswanth Alakkadan <aswanth682@gamil.com>
2022-08-10 06:31:19 +0530

    inital commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var stick = document.querySelector(".stick")
+window.onscroll = ()=>{
+    this.scrollY > 20 ? stick.classList.add("sticky"):
+    stick.classList.remove("sticky")
+}
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
